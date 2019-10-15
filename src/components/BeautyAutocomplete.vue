435ede00e238a51f12d5fc99cfc89c8bbd3772e6
Rafael Casachi <rafael@penze.com.br>
2019-10-15 15:13:49 -0300

    Created component for autocomplete field and switch button

---
+++ src/components/BeautyAutocomplete.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-autocomplete
+        :name="name"
+        :label="label"
+        :id="name"
+        :items="items"
+        v-bind:value="valueNew"
+        @input="valueComputed($event)"
+        item-color="red"
+        required
+        :dense="solo"
+        :solo="solo"
+    ></v-autocomplete>
+</template>
+
+<script>
+export default {
+    name: 'BeautyAutocomplete',
+    props: [
+        'items',
+        'name',
+        'label',
+        'value',
+        'solo'
+    ],
+    data() {
+        return {
+            valueNew: this.$props.value
+        };
+    },
+    methods: {
+        valueComputed: function($event) {
+            this.valueNew = $event;
+            this.$emit('input', $event);
+        }
+    }
+};
+</script>
---
+++ src/components/BeautySwitch.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-switch
+        :name="name"
+        :label="label"
+        :id="name"
+        color="red"
+        required
+        :dense="solo"
+        :solo="solo"
+        v-bind:value="valueNew"
+        @input="valueComputed($event)"
+    ></v-switch>
+</template>
+
+<script>
+export default {
+    name: 'BeautySwitch',
+    props: ['name', 'label', 'value', 'solo'],
+    data() {
+        return {
+            valueNew: this.$props.value
+        };
+    },
+    methods: {
+        valueComputed: function($event) {
+            this.valueNew = $event;
+            this.$emit('input', $event);
+        }
+    }
+};
+</script>
